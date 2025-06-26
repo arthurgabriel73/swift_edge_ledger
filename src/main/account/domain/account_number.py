fccf6ed2e5e6bba78b26617bc6bc0f7d58a09931
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-26 19:52:35 -0300

    test: fail 'test_account'

---
+++ src/main/account/domain/account_number.py
@@ -0,0 +1,3 @@
+class AccountNumber:
+    def __init__(self, account_number: str):
+        self.account_number = account_number
--- src/test/unit/account/test_account.py
+++ src/test/unit/account/test_account.py
@@ -4,6 +4,7 @@ import pytest
 
 from src.main.account.domain.account import Account
 from src.main.account.domain.account_id import AccountId
+from src.main.account.domain.account_number import AccountNumber
 from src.main.shared.date_util import get_utc_now
 
 
@@ -11,27 +12,31 @@ class TestAccount:
     def test_use_constructor_outside_class(self):
         # Act & Assert
         with pytest.raises(RuntimeError, match='Use the create method to instantiate an Account'):
-            Account(account_id = AccountId(uuid.uuid4()), created_at=get_utc_now())
+            Account(account_id=AccountId(uuid.uuid4()), account_number=AccountNumber('1232'), created_at=get_utc_now())
 
     def test_use_create_method(self):
         # Arrange
         account_id = AccountId(uuid.uuid4())
+        account_number = AccountNumber('123456789')
 
         # Act
-        account = Account.create(account_id)
+        account = Account.create(account_id, account_number)
 
         # Assert
         assert account.id == account_id
+        assert account.account_number == account_number
         assert account.created_at is not None
 
     def test_use_from_value_method(self):
         # Arrange
         account_id = AccountId(uuid.uuid4())
+        account_number = AccountNumber('123456789')
         created_at = get_utc_now()
 
         # Act
-        account = Account.from_value(account_id=account_id, created_at=created_at)
+        account = Account.from_value(account_id=account_id, account_number=account_number, created_at=created_at)
 
         # Assert
         assert account.id == account_id
+        assert account.account_number == account_number
         assert account.created_at == created_at
