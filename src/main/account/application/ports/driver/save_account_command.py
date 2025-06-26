1c84fbf4b5d9a0977d239c7fcb5a05fcfb35ca2e
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-26 19:56:25 -0300

    feat: save account driver port and command

---
+++ src/main/account/application/ports/driver/save_account_command.py
@@ -0,0 +1,3 @@
+class SaveAccountCommand:
+    def __init__(self, account_number: str):
+        self.account_number = account_number
---
+++ src/main/account/application/ports/driver/save_account_command_output.py
@@ -0,0 +1,4 @@
+class SaveAccountCommandOutput:
+    def __init__(self, account_id: str, account_number: str):
+        self.account_id = account_id
+        self.account_number = account_number
