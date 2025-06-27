ae1f1f4fc9e9f35aa658a63493e1d9132a4a5220
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-26 21:32:04 -0300

    fix: InMemoryAccountRepository

--- src/main/account/infra/adapters/driven/persistence/in_memory_account_repository.py
+++ src/main/account/infra/adapters/driven/persistence/in_memory_account_repository.py
@@ -1,23 +1,25 @@
-from typing import List
+from typing import List, Optional
 
 from src.main.account.application.ports.driven.account_repository import AccountRepository
 from src.main.account.domain.account import Account
+from src.main.account.domain.account_id import AccountId
+from src.main.account.domain.account_number import AccountNumber
 
 
 class InMemoryAccountRepository(AccountRepository):
     def __init__(self):
         self.accounts: List[Account] = []
 
-    def save(self, account) -> Account:
+    def save(self, account: Account) -> Account:
         self.accounts.append(self.to_new_instance(account))
         return self.to_new_instance(account)
 
-    def find_by_id(self, account_id):
-        found = next((account for account in self.accounts if account.id == account_id), None)
+    def find_by_id(self, account_id: AccountId) -> Optional[Account]:
+        found = next((account for account in self.accounts if account.id.string() == account_id.string()), None)
         return self.to_new_instance(found) if found else None
 
-    def find_by_account_number(self, account_number):
-        found = next((account for account in self.accounts if account.account_number == account_number), None)
+    def find_by_account_number(self, account_number: AccountNumber) -> Optional[Account]:
+        found = next((account for account in self.accounts if account.account_number.string() == account_number.string()), None)
         return self.to_new_instance(found) if found else None
 
     def to_new_instance(self, account: Account) -> Account:
