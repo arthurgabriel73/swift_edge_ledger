4b495f600cc8525004e76df5e961d38079a5f1bb
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-28 15:06:05 -0300

    refactor: changes category field name

--- src/main/merchant/domain/category.py
+++ src/main/merchant/domain/category.py
@@ -6,21 +6,21 @@ from src.main.shared.date_util import get_utc_now
 class Category:
     __allow_instantiation = False
 
-    def __init__(self, category_id: int, name: str, description: str, created_at: datetime):
+    def __init__(self, category_id: int, code: str, description: str, created_at: datetime):
         if not self.__allow_instantiation:
             raise RuntimeError('Use the create method to instantiate a Category')
 
         self.id = category_id
-        self.name = name
+        self.code = code
         self.description = description
         self.created_at = created_at
 
     @classmethod
-    def create(cls, category_id: int, name: str, description: str) -> 'Category':
+    def create(cls, category_id: int, code: str, description: str) -> 'Category':
         cls.__allow_instantiation = True
-        return cls(category_id, name, description, created_at=get_utc_now())
+        return cls(category_id, code, description, created_at=get_utc_now())
 
     @classmethod
-    def from_value(cls, *, category_id: int, name: str, description: str, created_at: datetime) -> 'Category':
+    def from_value(cls, *, category_id: int, code: str, description: str, created_at: datetime) -> 'Category':
         cls.__allow_instantiation = True
-        return cls(category_id, name, description, created_at)
+        return cls(category_id, code, description, created_at)
--- src/test/unit/merchant/test_category.py
+++ src/test/unit/merchant/test_category.py
@@ -8,35 +8,35 @@ class TestCategory:
     def test_use_constructor_outside_class(self):
         # Act & Assert
         with pytest.raises(RuntimeError, match='Use the create method to instantiate a Category'):
-            Category(category_id=123, name="FOOD", description="A food category", created_at=get_utc_now())
+            Category(category_id=123, code="FOOD", description="A food category", created_at=get_utc_now())
 
     def test_use_create_method(self):
         # Arrange
         category_id = 123
-        name = "FOOD"
+        code = "FOOD"
         description = "A food category"
 
         # Act
-        category = Category.create(category_id, name, description)
+        category = Category.create(category_id, code, description)
 
         # Assert
         assert category.id == category_id
-        assert category.name == name
+        assert category.code == code
         assert category.description == description
         assert category.created_at is not None
 
     def test_use_from_value_method(self):
         # Arrange
         category_id = 123
-        name = "FOOD"
+        code = "FOOD"
         description = "A food category"
         created_at = get_utc_now()
 
         # Act
-        category = Category.from_value(category_id=category_id, name=name, description=description, created_at=created_at)
+        category = Category.from_value(category_id=category_id, code=code, description=description, created_at=created_at)
 
         # Assert
         assert category.id == category_id
-        assert category.name == name
+        assert category.code == code
         assert category.description == description
         assert category.created_at == created_at
