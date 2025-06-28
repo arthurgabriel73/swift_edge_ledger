7271fb9622e93e1bb1fdd650c334e337492edbca
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-28 19:12:18 -0300

    feat: AccountBalance

---
+++ src/main/activity/domain/account_balance.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from uuid import UUID
+
+
+class AccountBalance:
+    __allow_instantiation = False
+
+    def __init__(self, *, account_balance_id: Optional[int], account_id: UUID, category_id: int, amount_in_cents: int, version: Optional[int]):
+        if not self.__allow_instantiation:
+            raise RuntimeError('Use the create method to instantiate an AccountBalance')
+
+        self.id = account_balance_id
+        self.account_id = account_id
+        self.category_id = category_id
+        self.amount_in_cents = amount_in_cents
+        self.version = version
+
+    @classmethod
+    def create(cls, account_id: UUID, category_id: int, amount_in_cents: int) -> 'AccountBalance':
+        cls.__allow_instantiation = True
+        return cls(account_balance_id=None, account_id=account_id, category_id=category_id, amount_in_cents=amount_in_cents, version=None)
+
+    @classmethod
+    def from_value(cls, *, account_balance_id: int, account_id: UUID, category_id: int, amount_in_cents: int, version: Optional[int]) -> 'AccountBalance':
+        cls.__allow_instantiation = True
+        return cls(account_balance_id=account_balance_id, account_id=account_id, category_id=category_id, amount_in_cents=amount_in_cents, version=version)
---
+++ src/test/unit/activity/test_account_balance.py
@@ -0,0 +1,46 @@
+import uuid
+
+import pytest
+
+from main.activity.domain.account_balance import AccountBalance
+
+
+class TestAccountBalance:
+    def test_use_constructor_outside_class(self):
+        # Act & Assert
+        with pytest.raises(RuntimeError, match='Use the create method to instantiate an AccountBalance'):
+            AccountBalance(account_balance_id=1, account_id=uuid.uuid4(), category_id=2, amount_in_cents=10000, version=None)
+
+    def test_use_create_method(self):
+        # Arrange
+        account_id = uuid.uuid4()
+        category_id = 2
+        amount_in_cents = 10000
+
+        # Act
+        account_balance = AccountBalance.create(account_id, category_id, amount_in_cents)
+
+        # Assert
+        assert account_balance.id is None
+        assert account_balance.account_id == account_id
+        assert account_balance.category_id == category_id
+        assert account_balance.amount_in_cents == amount_in_cents
+        assert account_balance.version is None
+
+    def test_use_from_value_method(self):
+        # Arrange
+        account_balance_id = 1
+        account_id = uuid.uuid4()
+        category_id = 2
+        amount_in_cents = 10000
+        version = 1
+
+        # Act
+        account_balance = AccountBalance.from_value(account_balance_id=account_balance_id, account_id=account_id, category_id=category_id, amount_in_cents=amount_in_cents, version=version)
+
+        # Assert
+        assert account_balance.id == account_balance_id
+        assert account_balance.account_id == account_id
+        assert account_balance.category_id == category_id
+        assert account_balance.amount_in_cents == amount_in_cents
+        assert account_balance.version == version
