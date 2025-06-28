26fbe0184b2debc46c06fe97ab3233ca81d03da6
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-28 15:36:42 -0300

    feat: SqlAlchemyCategoryRepository

---
+++ src/main/merchant/infra/adapters/driven/persistence/sqlalchemy/sql_alchemy_category_repository.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+from sqlalchemy import Table
+from sqlalchemy.future import select
+from sqlalchemy.orm import Session
+
+from main.merchant.application.ports.driven.category_repository import CategoryRepository
+from main.merchant.domain.category import Category
+from main.shared.database.sqlalchemy.models import CategoryEntity
+from main.shared.persistence_decorators import repository
+
+
+@repository
+class SqlAlchemyCategoryRepository(CategoryRepository):
+    def save(self, category: Category, session: Optional[Session] = None) -> Category:
+        if session is None:
+            raise ValueError("Session must be provided for saving the category.")
+
+        category_entity = CategoryEntity.from_domain(category)
+        table = Table('categories', CategoryEntity.metadata)
+        session.execute(table.insert(), [category_entity.to_dict()])
+        return category_entity.to_domain()
+
+    def find_by_id(self, category_id: int, session: Optional[Session] = None) -> Optional[Category]:
+        if session is None:
+            raise ValueError("Session must be provided for finding the category by ID.")
+
+        query = select(CategoryEntity).where(CategoryEntity.id == category_id)
+        category_entity = session.execute(query).scalars().unique().one_or_none()
+        if category_entity is None:
+            return None
+        return category_entity.to_domain()
+
+    def find_by_code(self, code: str, session: Optional[Session] = None) -> Optional[Category]:
+        if session is None:
+            raise ValueError("Session must be provided for finding the category by code.")
+
+        query = select(CategoryEntity).where(CategoryEntity.code == code)
+        category_entity = session.execute(query).scalars().unique().one_or_none()
+        if category_entity is None:
+            return None
+        return category_entity.to_domain()
