a0e94aa0d5f83ad425a3fa6b125983837c741fec
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-28 13:28:48 -0300

    feat: MccRepository

---
+++ src/main/merchant/application/ports/driven/mcc_repository.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from main.merchant.domain.mcc import Mcc
+from main.merchant.domain.mcc_id import MccId
+
+
+class MccRepository(ABC):
+    pass
+
+    @abstractmethod
+    def save(self, mcc: Mcc) -> Mcc:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, mcc_id: MccId) -> Optional[Mcc]:
+        pass
+
+    @abstractmethod
+    def find_by_code(self, code: str) -> Optional[Mcc]:
+        pass
