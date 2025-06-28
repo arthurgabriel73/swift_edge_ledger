e72e53282713d791c23f7e125660a0e2eee96ced
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-28 13:45:21 -0300

    test: pass test_use_from_value_method

--- src/test/unit/merchant/test_merchant.py
+++ src/test/unit/merchant/test_merchant.py
@@ -31,3 +31,18 @@ class TestMerchant:
         assert merchant.mcc_id == mcc_id
         assert merchant.created_at is not None
 
+    def test_use_from_value_method(self):
+        # Arrange
+        merchant_id = MerchantId(uuid.uuid4())
+        merchant_name = MerchantName("Test Merchant")
+        mcc_id = MccId(uuid.uuid4())
+        created_at = get_utc_now()
+
+        # Act
+        merchant = Merchant.from_value(merchant_id=merchant_id, merchant_name=merchant_name, mcc_id=mcc_id, created_at=created_at)
+
+        # Assert
+        assert merchant.id == merchant_id
+        assert merchant.name == merchant_name
+        assert merchant.mcc_id == mcc_id
+        assert merchant.created_at == created_at
