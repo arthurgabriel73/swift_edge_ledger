642198b03b1a825096b89b186226ae069bd203e7
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-29 20:47:39 -0300

    feat: adds exception handling to custom_exception_handler

--- src/main/shared/custom_exception_handler.py
+++ src/main/shared/custom_exception_handler.py
@@ -1,5 +1,9 @@
 from starlette.responses import JSONResponse
 
+from src.main.account.application.use_cases.exceptions.account_not_found_exception import AccountNotFoundException
+from src.main.activity.application.use_cases.exceptions.account_balance_conflict_exception import \
+    AccountBalanceConflictException
+from src.main.merchant.application.use_cases.exceptions.category_not_found_exception import CategoryNotFoundException
 from src.main.merchant.application.use_cases.exceptions.category_conflict_exception import CategoryConflictException
 from src.main.merchant.application.use_cases.exceptions.mcc_conflict_exception import MccConflictException
 from src.main.merchant.application.use_cases.exceptions.merchant_conflict_exception import MerchantConflictException
@@ -39,5 +43,26 @@ async def mcc_conflict_exception_handler(request, exc):
 async def category_conflict_exception_handler(request, exc):
     return JSONResponse(
         status_code=409,
+        content={"detail": str(exc.message)}
+    )
+
+@app.exception_handler(AccountBalanceConflictException)
+async def account_balance_conflict_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=409,
+        content={"detail": str(exc.message)}
+    )
+
+@app.exception_handler(AccountNotFoundException)
+async def account_not_found_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=404,
+        content={"detail": str(exc.message)}
+    )
+
+@app.exception_handler(CategoryNotFoundException)
+async def category_not_found_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=404,
         content={"detail": str(exc.message)}
     )
