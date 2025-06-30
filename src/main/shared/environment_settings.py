80de0d4cec673dccd69702c961bbe96fe0d1fd17
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-30 04:04:53 -0300

    fix: fixes default env

--- src/main/shared/environment_settings.py
+++ src/main/shared/environment_settings.py
@@ -14,10 +14,10 @@ def get_env_filename():
 
 class EnvironmentSettings(BaseSettings):
     APP_NAME: Optional[str] = Field(validation_alias='APP_NAME_VALUE') or 'swift_edge_ledger'
-    APP_HOST: Optional[str] = Field(validation_alias='APP_HOST_VALUE') or '0.0.0.0'
+    APP_HOST: Optional[str] = Field(validation_alias='APP_HOST_VALUE') or 'localhost'
     APP_PORT: Optional[int] = Field(validation_alias='APP_PORT_VALUE') or '3000'
     DATABASE_DIALECT: Optional[str] = Field(validation_alias='DATABASE_DIALECT_VALUE') or 'postgresql+psycopg'
-    DATABASE_HOST: Optional[str] = Field(validation_alias='DATABASE_HOST_VALUE') or 'localhost'
+    DATABASE_HOST: Optional[str] = Field(validation_alias='DATABASE_HOST_VALUE') or 'postgres'
     DATABASE_NAME: Optional[str] = Field(validation_alias='DATABASE_NAME_VALUE') or 'swift_edge_ledger_db'
     DATABASE_PASSWORD: Optional[str] = Field(validation_alias='DATABASE_PASSWORD_VALUE') or 'password'
     DATABASE_PORT: Optional[int] = Field(validation_alias='DATABASE_PORT_VALUE') or '5432'
